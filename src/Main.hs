06f35e088f9d953fdd1178a450c00783780748e1
Eric Litak <elitak@gmail.com>
2015-03-17 00:14:19 -0700

    refactored regex out to new module

--- src/Main.hs
+++ src/Main.hs
@@ -7,87 +7,9 @@ import Shelly hiding (fromText)
 import System.Environment
 import Data.Maybe
 import Data.Text (pack, unpack, intercalate, append, lines)
+import StatusParser
 import Text.Regex.Applicative
-import Data.Char
 
--- XXX TODO Maybe return from (=~) is all well and good until `read` fails on an int...
---          How best to handle this? A better way to invoke than =~ ?
-type TorrentId = Integer
-type Faulty = Bool
-data DonePct = DoneNA
-             | DonePct Integer deriving (Eq, Show)
-data Have = NoHave
-          | Have { haveAmount :: Float
-                 , haveUnits  :: String } deriving Show
-data ETA = Done
-         | Unknown
-         | ETA { etaAmount :: Integer
-               , etaUnits  :: String }
-         | OtherETA String deriving Show
-type Up = Float
-type Down = Float
-data Ratio = Ratio Float
-           | NoRatio deriving Show
-data Status = Bidirectional
-            | Idle
-            | Seeding
-            | Stopped
-            | Queued
-            | Verifying
-            | WillVerify
-            | OtherStatus String deriving Show
-type Name = String
--- TODO move to own module and the remove sl prefix and expot
-data StatusLine = StatusLine { slId :: TorrentId
-                             , slFaulty :: Faulty
-                             , slDone :: DonePct
-                             , slHave :: Have
-                             , slEta :: ETA
-                             , slUp :: Up
-                             , slDown :: Down
-                             , slRatio :: Ratio
-                             , slStatus :: Status
-                             , slName :: Name
-                             } deriving Show
-
-statusLine = StatusLine <$  spaces
-                        <*> torrentId
-                        <*> faulty <* spaces
-                        <*> donePct <* spaces
-                        <*> have <* spaces
-                        <*> eta <* spaces
-                        <*> up <* spaces
-                        <*> down <* spaces
-                        <*> ratio <* spaces
-                        <*> status <* spaces
-                        <*> name <* spaces
-    where spaces = many (psym isSpace)
-          intfmt = liftA read $ many $ psym isDigit
-          floatfmt = liftA read $ many $ psym (\x -> isDigit x || (== '.') x)
-          torrentId = intfmt
-          faulty = True <$ string "*"
-               <|> pure False
-          donePct = DoneNA <$ string "n/a"
-                <|> DonePct <$> (liftA read $ many $ psym isDigit) <* string "%"
-          have = NoHave <$ string "None"
-             <|> Have <$> floatfmt <* psym isSpace <*> many (psym isAlpha)
-          eta = Done <$ string "Done"
-            <|> Unknown <$ string "Unknown"
-            <|> ETA <$> intfmt <* psym isSpace <*> many (psym isAlpha)
-            <|> OtherETA <$> many (psym isAlpha)
-          up = floatfmt
-          down = floatfmt
-          ratio = NoRatio <$ string "None"
-              <|> Ratio <$> floatfmt
-          status = Bidirectional <$ string "Up & Down"
-               <|> Idle <$ string "Idle"
-               <|> Seeding <$ string "Seeding"
-               <|> Stopped <$ string "Stopped"
-               <|> Queued <$ string "Queued"
-               <|> Verifying <$ string "Verifying"
-               <|> WillVerify <$ string "Will Verify"
-               <|> OtherStatus <$> many (psym isAlpha)
-          name = many anySym
 
 -- TODO replace shelly with shell-conduit, once I get that working.
 xm_bin = "transmission-remote"
@@ -114,9 +36,9 @@ getFinishedIds = do
     let _:skipFooter = (reverse . lines) tors
     let _:skipHeader = reverse skipFooter
     let fin = filter isFinished $ map parse skipHeader
-    return $ map (pack . show . slId) fin
+    return $ map (pack . show . tid) fin
     where parse = (fromJust . (flip (=~) statusLine) . unpack)
-          isFinished StatusLine{..} = not slFaulty && slDone == DonePct 100
+          isFinished StatusLine{..} = not faulty && done == DonePct 100
 {- TODO
 xmclean() {
     local XM_TORS="/home/keb/.config/transmission-daemon/torrents"
---
+++ src/StatusParser.hs
@@ -0,0 +1,94 @@
+module StatusParser ( StatusLine(..)
+                    , statusLine
+                    -- TODO do i need type aliases exported?
+                    --, TorrentId
+                    --, Faulty
+                    , DonePct(..)
+                    , ETA(..)
+                    --, Up
+                    --, Down
+                    , Ratio(..)
+                    , Status(..)
+                    --, Name
+) where
+
+import Text.Regex.Applicative
+import Data.Char
+
+-- XXX TODO Maybe return from (=~) is all well and good until `read` fails on an int...
+--          How best to handle this? A better way to invoke than =~ ?
+type TorrentId = Integer
+type Faulty = Bool
+data DonePct = DoneNA
+             | DonePct Integer deriving (Eq, Show)
+data Have = NoHave
+          | Have { haveAmount :: Float
+                 , haveUnits  :: String } deriving Show
+data ETA = Done
+         | Unknown
+         | ETA { etaAmount :: Integer
+               , etaUnits  :: String }
+         | OtherETA String deriving Show
+type Up = Float
+type Down = Float
+data Ratio = Ratio Float
+           | NoRatio deriving Show
+data Status = Bidirectional
+            | Idle
+            | Seeding
+            | Stopped
+            | Queued
+            | Verifying
+            | WillVerify
+            | OtherStatus String deriving Show
+type Name = String
+data StatusLine = StatusLine { tid :: TorrentId
+                             , faulty :: Faulty
+                             , done :: DonePct
+                             , have :: Have
+                             , eta :: ETA
+                             , up :: Up
+                             , down :: Down
+                             , ratio :: Ratio
+                             , status :: Status
+                             , name :: Name
+                             } deriving Show
+
+statusLine = StatusLine <$  spaces
+                        <*> torrentId
+                        <*> faulty <* spaces
+                        <*> donePct <* spaces
+                        <*> have <* spaces
+                        <*> eta <* spaces
+                        <*> up <* spaces
+                        <*> down <* spaces
+                        <*> ratio <* spaces
+                        <*> status <* spaces
+                        <*> name <* spaces
+    where spaces = many (psym isSpace)
+          intfmt = liftA read $ many $ psym isDigit
+          floatfmt = liftA read $ many $ psym (\x -> isDigit x || (== '.') x)
+          torrentId = intfmt
+          faulty = True <$ string "*"
+               <|> pure False
+          donePct = DoneNA <$ string "n/a"
+                <|> DonePct <$> (liftA read $ many $ psym isDigit) <* string "%"
+          have = NoHave <$ string "None"
+             <|> Have <$> floatfmt <* psym isSpace <*> many (psym isAlpha)
+          eta = Done <$ string "Done"
+            <|> Unknown <$ string "Unknown"
+            <|> ETA <$> intfmt <* psym isSpace <*> many (psym isAlpha)
+            <|> OtherETA <$> many (psym isAlpha)
+          up = floatfmt
+          down = floatfmt
+          ratio = NoRatio <$ string "None"
+              <|> Ratio <$> floatfmt
+          status = Bidirectional <$ string "Up & Down"
+               <|> Idle <$ string "Idle"
+               <|> Seeding <$ string "Seeding"
+               <|> Stopped <$ string "Stopped"
+               <|> Queued <$ string "Queued"
+               <|> Verifying <$ string "Verifying"
+               <|> WillVerify <$ string "Will Verify"
+               <|> OtherStatus <$> many (psym isAlpha)
+          name = many anySym
