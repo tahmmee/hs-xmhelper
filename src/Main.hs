6b34bdcedeff2c0998ea5ec9944c87a89133e34b
Eric Litak <elitak@gmail.com>
2015-08-30 21:43:11 -0700

    took forever but i sorta understand turtle now

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,8 @@ import System.Environment
 import Data.Maybe
 import Data.Text (pack, unpack, intercalate, lines, unlines, strip, Text)
 import Data.Text.IO (putStr, putStrLn)
+--import Data.Foldable (toList)
+import qualified Control.Foldl as Foldl
 
 import StatusParser
 import Text.Regex.Applicative
@@ -15,53 +17,66 @@ import Text.Regex.Applicative
 import TransmissionConfig
 import Control.Monad
 
+-- TODO remove this convenience func i added at the start of turtle porting
 run :: Text -> [Text] -> Shell Text
 run cmd args = inshell (intercalate " " $ [cmd] <> args) empty
 
 xm :: [Text] -> Shell Text
 xm args = run "transmission-remote" args
 
-xmOn :: [Integer] -> [Text] -> Shell Text
-xmOn [] _ = return "Nothing to do (No torrent ids given on which to operate).\n"
-xmOn ids args = xm $ ["-t" <> intercalate "," (map (pack . show) ids)] <> args
+xmOn :: Shell Integer -> [Text] -> Shell Text
+xmOn ids args = do
+    idList <- fold ids Foldl.list
+    case idList of
+        [] -> "Nothing to do; no torrent ids given."
+        _  -> do
+             let idts = map (pack . show) idList
+             xm $ ["-t" <> intercalate "," idts] <> args
 
 xmo args = do
     let op:ids = args
-    xmOn (map (read . unpack) ids) [op]
+    xmOn (select $ map (read . unpack) ids) [op]
 
 xmcheck args = do
-    ids <- getFinishedIds
-    xmOn ids ["-v"]
+    xmOn getFinishedIds ["-v"]
 
 xmf args = do
-    ids <- getFinishedIds
-    xmOn ids ["-l"]
+    xmOn getFinishedIds ["-l"]
 
 xmclean args = do
     TransmissionConfig{..} <- liftIO getConfig
     src <- liftIO getConfigDir
     let dest = downloadDir <> "/_torrents"
-    out <- run "rsync" ["-rP", (src <> "/torrents/"), dest]
+    run "rsync" ["-rP", (src <> "/torrents/"), dest]
     -- TODO check rsync is in path
-    liftIO $ putStr out
     -- TODO Guard here to make sure the above executed correctly (currently failure exits uncleanly)
-    ids <- getFinishedIds
-    xmOn ids ["-r"]
+    xmOn getFinishedIds ["-r"]
 
--- XXX TODO handle exception on parse returning Nothing
--- XXX skip first and last line better (just handle and skip Nothing?)
+-- TODO handle exception on parse returning Nothing
+getFinishedIds :: Shell Integer
 getFinishedIds = do
-    tors <- xm ["-l"]
-    return [ tid parsed | line <- body tors
-           , let parsed = parse line
-           , isFinished parsed ]
-    where parse = fromJust . (=~ statusLine) . unpack
-          body = tail . reverse . tail . reverse . lines  -- strip first and last line
+    lines <- parse <$> xm ["-l"]
+    --lines <- parse <$> inshell "cat sample" empty
+    case isFinished lines of
+        True -> tid <$> pure lines
+        False -> empty
+    --TODO write like this--tid <$> isFinished <$> parse <$> inshell "cat sample" empty
+
+    where
+          body = tail . reverse . tail . reverse -- strip first and last line
+          -- XXX fromJust should never fail since failed parse returns its own data constructor. still a better way to write in applicative context?
+          parse = fromJust . (=~ statusLine) . unpack
           isFinished StatusLine{..} = not faulty && done == DonePct 100
+          --isFinished x@StatusLine{..}
+          --  | not faulty && done == DonePct 100 = True
+          --  | otherwise = False
+          isFinished _ = False
 
 xmtest args = do
     -- test whatever here
-    return "test"
+    --return "test"
+    ids <- getFinishedIds
+    return $ pack . show $ ids
     
 
 -- TODO replace the lookup with template-haskell or something
